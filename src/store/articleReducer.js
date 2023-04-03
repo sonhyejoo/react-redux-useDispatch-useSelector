566ab9642eed04b8492cbca031a726b99060551f
Son Hyejoo <107318155+sonhyejoo@users.noreply.github.com>
2023-04-02 22:59:56 -0400

    done addArticle and dispatch

--- src/store/articleReducer.js
+++ src/store/articleReducer.js
@@ -1,11 +1,18 @@
-import articles from '../data/data.json';
+import articles from "../data/data.json";
 
-const LOAD_ARTICLES = 'article/loadArticles';
+const LOAD_ARTICLES = "article/loadArticles";
+const ADD_ARTICLE = "article/addArticle";
 
 export const loadArticles = () => {
   return {
     type: LOAD_ARTICLES,
     articles
+  };
+};
+export const addArticle = () => {
+  return {
+    type: ADD_ARTICLE,
+    article
   };
 };
 
@@ -15,6 +22,10 @@ const articleReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_ARTICLES:
       return { ...state, entries: [...action.articles] };
+    case ADD_ARTICLE:
+      const newState = state;
+      newState.entries.append(action.article);
+      return newState;
     default:
       return state;
   }
